b46ee125231f523a0284c38e7f5cabed5729ef73
antonyjes <72809149@continental.edu.pe>
2024-08-21 14:39:08 -0500

    date format function

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 
 def load_json(filename):
     with open(filename, 'r') as file:
@@ -7,3 +8,6 @@ def load_json(filename):
 def save_json(filename, data):
     with open(filename, 'w') as file:
         json.dump(data, file, indent=2)
+
+def date_format(date):
+    return datetime.datetime.strptime(date, '%Y-%m-%d')
