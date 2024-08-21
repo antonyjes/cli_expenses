dcf591af7171ea3dc87f0bf2aa29f00fc1dd32a6
antonyjes <72809149@continental.edu.pe>
2024-08-21 14:38:59 -0500

    summary with year as option

--- summary.py
+++ summary.py
@@ -1,12 +1,21 @@
 import click
-from utils import load_json
+from utils import load_json, date_format
 
+# Total expenses
 @click.command()
-def summary():
+@click.option('--year', default=None, required=False)
+def summary(year):
     total_expenses = 0
     data = load_json('expenses.json')
 
     for expense in data['expenses']:
-        total_expenses += expense['amount']
-
-    click.echo(f"Total expenses: {total_expenses}")
+        if year:
+            if date_format(expense['date']).year == int(year):
+                total_expenses += expense['amount']
+        else:
+            total_expenses += expense['amount']
+    
+    if year:
+        click.echo(f"Total expenses in {year}: {total_expenses}")
+    else:
+        click.echo(f"Total expenses: {total_expenses}")
