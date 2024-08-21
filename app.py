1c0f5872e78de9a59e19a718f5e31c818f38bf38
antonyjes <72809149@continental.edu.pe>
2024-08-21 14:42:40 -0500

    summary command to the main app

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import click
 from crud import add, list, delete
+from summary import summary, year
 
 @click.group()
 def cli():
@@ -9,6 +10,9 @@ def cli():
 cli.add_command(add)
 cli.add_command(list)
 cli.add_command(delete)
+
+# Summary command
+cli.add_command(summary)
 
 if __name__ == '__main__':
     cli()
